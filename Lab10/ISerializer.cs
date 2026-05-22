90babe62d25003d29b2c1e80448b52b72e34409a
Ver-Vina <vervina7@mail.ru>
2026-05-22 10:35:15 +0300

    Create ISerializer.cs

---
+++ Lab10/ISerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Lab10.Purple
+{
+    public interface ISerializer<T> where T : Lab9.Purple.Purple
+    {
+        T Deserialize();
+        void Serialize(T obj);
+    }
+}
