17f215e901fb0e2e64c6b90775e559ac4d4fa595
Ver-Vina <vervina7@mail.ru>
2026-05-22 10:33:56 +0300

    Create IFileLifeController.cs

---
+++ Lab10/IFileLifeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Purple
+{
+    public interface IFileLifeController
+    {
+        void CreateFile();
+        void DeleteFile();
+        void EditFile(string filename);
+        void ChangeFileExtension(string ext);
+    }
+}
